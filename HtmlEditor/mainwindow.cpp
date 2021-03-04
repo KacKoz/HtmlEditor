754d6c8e0f9ad9441236296d093cc912a3f07e77
Kacper <kacpro@onet.com.pl>
2021-03-04 18:33:15 +0100

    dalej scalam

--- HtmlEditor/mainwindow.cpp
+++ HtmlEditor/mainwindow.cpp
@@ -115,6 +115,7 @@ void MainWindow::on_actionSave_as_triggered()
 {
     setWindowTitle(windowTitle().replace(0,1,""));
     hasChanged=false;
+    1;
     QString filename = QFileDialog::getSaveFileName(this,"Save as");
     QFile file(filename);
     if(!file.open(QIODevice::WriteOnly | QFile::Text)){
